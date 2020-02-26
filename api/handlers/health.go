a0272ddc944c60de5b525507012f99907e969d4a
FernandoCagale <fernando.b.cagale@gmail.com>
2020-02-26 20:21:47 -0300

    add health to rabbitmq and postgresql

--- api/handlers/health.go
+++ api/handlers/health.go
@@ -1,17 +1,38 @@
 package handlers
 
 import (
-	"github.com/FernandoCagale/c4-customer/api/render"
+	"github.com/hellofresh/health-go"
+	"os"
+	"time"
+
+	healthRabbitmq "github.com/hellofresh/health-go/checks/rabbitmq"
+	healthPostgres "github.com/hellofresh/health-go/checks/postgres"
 	"net/http"
 )
 
 type HealthHandler struct {
 }
 
-func (handler *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
-	render.Response(w, map[string]bool{"ok": true}, http.StatusOK)
-}
-
 func NewHealth() *HealthHandler {
 	return &HealthHandler{}
+}
+
+func (handler *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
+	health.Register(health.Config{
+		Name:    "rabbitmq",
+		Timeout: time.Second * 5,
+		Check: healthRabbitmq.New(healthRabbitmq.Config{
+			DSN: os.Getenv("AMQP_URI"),
+		}),
+	})
+
+	health.Register(health.Config{
+		Name:    "postgresql",
+		Timeout: time.Second * 5,
+		Check: healthPostgres.New(healthPostgres.Config{
+			DSN: os.Getenv("POSTGRES_ADDRS"),
+		}),
+	})
+
+	health.HandlerFunc(w, r)
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/google/wire v0.3.0
 	github.com/gorilla/mux v1.7.3
+	github.com/hellofresh/health-go v2.0.2+incompatible
 	github.com/jinzhu/gorm v1.9.11
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.2.0
