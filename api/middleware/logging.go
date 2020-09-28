aa3382721317d2c47ee16691dbab11ecb76c74a4
FernandoCagale <fernando.b.cagale@gmail.com>
2020-09-28 08:55:07 -0300

    add middleware logging

---
+++ api/middleware/logging.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
